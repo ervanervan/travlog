05e8db940e791713a50cc42607bd510949b53b21
ervanervan <ervanervan040@gmail.com>
2023-10-22 15:50:39 +0700

    Finished creating destinations on mobile display

--- src/components/CardDestinations.jsx
+++ src/components/CardDestinations.jsx
@@ -12,11 +12,11 @@ export default function CardDestinations({
     <div className="w-80 bg-bgWhite rounded-3xl shadow-lg">
       <img src={imgUrl} alt="destination" className="w-full" />
       <div className="p-8">
-        <h3 className="text-textSecondary font-bold text-xl mb-2">{price}</h3>
-        <h2 className="text-textBlack font-bold text-xl mb-4">{title}</h2>
-        <p className="text-textBlack75 mb-8">{location}</p>
+        <h3 className="text-textSecondary font-bold text-2xl mb-2">{price}</h3>
+        <h2 className="text-textBlack font-bold text-2xl mb-4">{title}</h2>
+        <p className="text-textBlack75 text-lg mb-8">{location}</p>
         <p className="flex gap-2 items-center">
-          <span className="text-textOrange font-bold text-xl">{rating}</span>
+          <span className="text-textOrange font-bold text-2xl">{rating}</span>
           <img src={icon} alt="" width={24} height={24} />
         </p>
       </div>
