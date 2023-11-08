425023dc9ae6491eca5314015a6f491f87df2d8c
ervanervan <ervanervan040@gmail.com>
2023-11-08 20:43:26 +0700

    Finished update data statis card destination and card testimonial

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -25,7 +25,7 @@ export default function Testimonial() {
 
   return (
     <section className="container mx-auto">
-      <div className=" py-8">
+      <div className="py-8">
         <div className="text-center">
           <h3 className="font-bold text-textSecondary text-base uppercase tracking-[2px] mb-4">
             Testimonials
@@ -35,12 +35,12 @@ export default function Testimonial() {
           </h2>
         </div>
         <Carousel
-          // swipeable={false}
+          swipeable={false}
           showArrows={false}
           showStatus={false}
           showThumbs={false}
           infiniteLoop={true}
-          autoPlay={false}
+          autoPlay={true}
           selectedItem={activeIndex}
         >
           {DATA_TESTIMONIALS.map((testimonial) => {
