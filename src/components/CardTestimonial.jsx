6654db45c3a8a87ecbe3e541f8330c494649c361
ervanervan <ervanervan040@gmail.com>
2023-11-08 20:47:40 +0700

    Finished update data statis card destination and card testimonial

--- src/components/CardTestimonial.jsx
+++ src/components/CardTestimonial.jsx
@@ -8,7 +8,7 @@ export default function CardTestimonial({
   imgUrl,
 }) {
   return (
-    <div className="pt-8 pb-16 w-[22rem] pl-6">
+    <div className="pt-8 pb-16 w-[22rem]">
       <div className="flex flex-col items-center justify-center gap-16">
         <div className="w-32 h-32 rounded-full">
           <img
